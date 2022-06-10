7e0af1faf0d20737247a2a28cf1d9aeaa29da5ee
Liseth Ponce <lka.ponce@yavirac.edu.ec>
2022-06-10 14:27:50 -0500

    Migraciones con TYPEORM

---
+++ src/entity/User.ts
@@ -0,0 +1,26 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    firstName: string
+
+    @Column()
+    lastName: string
+
+    @Column()
+    age: number
+
+    @Column()
+    course: string
+
+    @Column()
+    telephone:number
+
+    @Column()
+    address:number
+}
